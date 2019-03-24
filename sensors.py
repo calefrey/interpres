ccd5b112c043cfc8e78cb1bb598f4f8c4ea4c6e5
calefrey <calefrey@gmail.com>
2019-03-24 01:02:12 -0400

    Placeholder for script to handle arduino logic

---
+++ sensors.py
@@ -0,0 +1,5 @@
+def read():  #should return a 1d array of sensor values
+    """
+    Returns an array of the current sensor data from tue arduino
+    """
+    return [1,2,3,4,5,6,7,8,9,0] # used for testing
